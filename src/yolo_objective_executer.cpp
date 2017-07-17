9d37bb885aa77ac7967223e690cc3f27475f6c97
[Vinh Nguyen] <[vnguye34@students.kennesaw.edu]>
2017-07-17 19:25:29 -0400

    initial commit

---
+++ src/yolo_objective_executer.cpp
@@ -0,0 +1,11 @@
+#include "ros/ros.h"
+#include <ros/console.h>
+#include <iostream>
+
+using namespace std
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "yolo_objective_executer");
+    ros::NodeHandle nh;
+}
