6f26d8708956cce50e1b50a1b8ac3283006132e0
shadySource <shadySource2@gmail.com>
2017-07-23 16:56:16 -0700

    update objective functions

--- include/objective_definitions.h
+++ include/objective_definitions.h
@@ -7,6 +7,35 @@ Purpose: provide common definitions tobe used with objects
 #ifndef OBJECTIVE_DEFINITIONS
 #define OBJECTIVE_DEFINITIONS
 
-#define EXAMPLE_OBJECT  0
+#define START_GATE      0
+
+#define PATH_MARKER     1
+
+#define RED_BUOY        2
+#define GREEN_BUOY      3
+#define YELLOW_BUOY     4
+
+#define CHANNEL         5
+
+#define DROPPER_BIN     6
+#define DROPPER_HANDLE  7
+
+#define SQUID_HOLE      8
+#define SEAWEED         9
+
+#define PINGER          10
+
+#define RED_SAMPLE      11
+#define GREEN_SAMPLE    12
+#define ORANGE_SAMPLE   13
+#define BLUE_SAMPLE     20
+
+#define RED_TABLE       21
+#define GREEN_TABLE     22
+#define ORANGE_TABLE    23
+#define BLUE_TABLE      24
+
+#define OCTAGON         25
+#define BEACH_BALL      26
 
 #endif
