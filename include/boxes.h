68f25ba842a0b1a91fdd25773a410b157ecfffed
shadySource <shadySource2@gmail.com>
2017-07-23 19:31:22 -0400

    update boxes

--- include/boxes.h
+++ include/boxes.h
@@ -8,14 +8,66 @@ Purpose: class to manage active boxes
 #ifndef BOXES_DEF
 #define BOXES_DEF
 #include <ros/ros.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <vector>
+#include <math.h>
 
 namespace boxes
 {
 
 class Boxes
 {
-    //TODO
+    private:
+        //@var nodehandle for boxes subscriber
+        ros::NodeHandle _nh;
+
+        //@var subscriber to yolo boxes for first cam
+        ros::Subscriber _boxesSub;
+
+        //@var subscriber for second camera boxes
+        ros::Subscriber _secondBoxesSub;
+
+        //@var boxes holding data structure
+        float ** _boxes;
+        int _numBoxes;
+        bool _newBoxes;
+
+        /*
+        Callback for the first boxes message
+        */
+        void BoxesCallback(const std_msgs::Float32MultiArray &msg);
+
+
     public:
+        /*
+        find if boxes contains a certain object
+
+        @param object: int of object to search for
+        @param camera: 1 for first camera, 2 for second camera
+
+        @return bool true/false where object contained
+        */
+        bool Contains(int object);
+
+        /*
+        find if boxes contains new information
+
+        @param camera: 1 for first camera, 2 for second camera
+
+        @return bool true/false where object contained
+        */
+        bool NewBox();
+
+        /**
+        finds a box of a certain type closest to the center.
+
+        @param object: int object to find the  best box for
+
+        @return center pointer to center x coordinate of a box with 
+        a certain type closest to the center. y coord is one mem aaddress over.
+        */
+        float * GetNearest(int object);
+
 
     Boxes();
 
--- src/boxes.cpp
+++ src/boxes.cpp
@@ -7,7 +7,75 @@ Purpose: source file for Boxes object
 */
 #include "boxes.h"
 
+using namespace boxes;
+
 Boxes::Boxes()
+    : _newBoxes(false), _numBoxes(0)
 {
-    //TODO
+    _boxesSub = _nh.Subscribe("yolo/first/boxes", 10, &Boxes::boxesCallback, this);
+    _boxes = new float[100][8];
+}
+
+Boxes::~Boxes()
+{
+    delete _boxes;
+}
+
+void Boxes::BoxesCallback(const std_msgs::Float32MultiArray &msg)
+{
+    _numFirstBoxes = msg.layout.dim[0].shape;
+    for (int i(0); i < _numFirstBoxes; i++)
+    {
+        _boxes[i][0] = msg.data[i*6]; // class
+        _boxes[i][1] = msg.data[i*6+5]; //confidence
+        _boxes[i][2] = msg.data[i*6+1]; //ymin, 0 to 1   
+        _boxes[i][3] = msg.data[i*6+2]; //xmin
+        _boxes[i][4] = msg.data[i*6+3]; //yMax
+        _boxes[i][5] = msg.data[i*6+4]; //xMax
+        _boxes[i][6] = (_boxes[i][3] + _boxes[i][5])/2 - 0.5; //xCenter, -.5 to .5
+        _boxes[i][7] = (_boxes[i][2] + _boxes[i][4])/2 - 0.5; //yCenter, -.5 to .5
+    }
+    _newBoxes = true;
+}
+
+bool Boxes::NewBox()
+{
+    _newBoxes = False;
+    return _newBoxes;
+}
+
+bool Boxes::Contains(int object)
+{
+    for (int i(0); i < _numBoxes; i++)
+    {
+        if (round(_boxes[i][0]) == object)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+float * GetNearest(int object)
+{
+    int idx;
+    int err(1);
+    for (int i(0); i < _numBoxes; i++)
+    {
+        if (round(_boxes[i][0]) == object &&
+        sqrt(pow(_boxes[i][6], 2) + pow(_boxes[i][7], 2)) < err)
+        {
+            idx = i;
+        }
+    }
+
+    if (err < 1)
+    {
+        return &(_boxes[idx][6]);
+    }
+    else
+    {
+        return &NULL;
+    }
 }
