b076f16c6398d4ec6782fd32bb22c34b5a0269e7
vnguye34 <vnguye34@students.kennesaw.edu>
2017-07-24 19:45:48 -0700

    work on dropper
    add logic tree

--- src/dropper.cpp
+++ src/dropper.cpp
@@ -10,9 +10,9 @@ Purpose: source file for dropper
 using namespace state;
 
 Dropper::Dropper(ros::Publisher &motionPub, boxes::Boxes& boxes)
-     : State::State(&motionPub, &boxes), _target(DROPPER_BIN)
+     : State::State(&motionPub, &boxes), _target(DROPPER_HANDLE)
 {
-    MotionMessage[0] = 1; //mode 3, dropper movement
+    MotionMessage[0] = 4; //mode 4, dropper movement
 	MotionMessage[1] = 0; //x bottom cam
 	MotionMessage[2] = 0; //y bottom cam
     MotionMessage[3] = 0; //forward thrust, -1, 1, forward backward
@@ -26,50 +26,30 @@ int Dropper::Execute()
 	
     while(ros::ok())
     {
-		switch(_target)
-		{
-			case RED_BUOY:
-			if (Boxes->NewBox() && Boxes->Contains(RED_BOUY)):
+				//find box for dropper lid
+				//go until center of camera = centroid of dropper lid
+				//move backward offset to grabber arm + error value
+				//send signal to servo to open grabber arm
+				//move forward to position of grabber arm + error value
+				//send signal to servo to close grabber arm
+				//move sub forward until bottom camera does not see dropper lid
+				//send signal to servo to let go of lid
+				//switch mode into depth_hold and tilt sub to let lid dropper
+				//move sub backwards until uncovered dropper bin is back in view of bottom facing camera
+				//move offset to uncovered bin
+				//send signal to servo to drop 2 droppers
+				//end state
+			if (Boxes->NewBox() && Boxes->Contains(DROPPER_HANDLE):
 			{
+				//idenify target and move towards centroid 
 				this->UpdateTarget();
 				MotionPub.publish(this->MotionMessage);
-				lastSeen = ros::Time::now().toSec();
+				
 			}
 			else if (lastSeen > 10)
 			{
 				_target = GREEN_BUOY;
 			}
-			break;
-			
-			case GREEN_BOUY:
-			lastSeen = -1;
-			if(Boxes->NewBox() && Boxes->Contains(GREEN_BOUY)):
-			{
-				this->UpdateTarget();
-				MotionPub.publish(this->MotionMessage);
-				lastSeen = ros::Time::now().toSec();
-			}
-			else if (lastSeen > 10)
-			{
-				_target = YELLOW_BOUY;
-			}
-			break;
-			
-			case YELLOW_BOUY:
-			lastSeen = -1;
-			if(Boxes->NewBox() && Boxes->Contains(YELLOW_BOUY)):
-			{
-				this->UpdateTarget();
-				MotionPub.publish(this->MotionMessage);
-				lastSeen = ros::Time::now().toSec();
-			}
-			else if (lastSeen > 10)
-			{
-				_target = PATH_MARKER;	
-			}
-			break;
-        }
-		}
         ros::spinOnce();
         SleepRate.sleep();
     }
@@ -78,5 +58,6 @@ int Dropper::Execute()
 void Bouy::UpdateTarget()
 {
     std::vector target(Boxes->GetNearest(_target));
-    this->MotionMessage[0] = target[0];
+    this->MotionMessage[0] = target[0]; //x
+	this->MotionMessage[1] = target[1]; //y
 }
